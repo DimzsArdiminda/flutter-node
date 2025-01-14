c385fdc3df6e8c5f72d62341918aa3e009b3c314
Dimas Ardiminda <113449449+DimzsArdiminda@users.noreply.github.com>
2025-01-15 06:01:14 +0700

    feat: dashboard

---
+++ FrontEnd/lib/main/components/button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ButtonDashboardShowList extends StatelessWidget {
+  const ButtonDashboardShowList({
+    super.key,
+    required this.menu,
+  });
+
+  final Map<String, dynamic> menu;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        // foregroundColor: Colors.black,
+        // elevation: 2,
+        padding: EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      onPressed: () {
+        print("Anda memilih menu ${menu['Nama']}");
+      },
+      child: ListTile(
+        leading: Image.asset(
+          menu['path_image'],
+          width: 100,
+          height: 100,
+        ),
+        title: Text(menu['Nama'],
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text('Porsi: ${menu['porsi']}'),
+        trailing: Text('Harga: ${menu['harga']}'),
+      ),
+    );
+  }
+}
--- FrontEnd/lib/main/dashboard.dart
+++ FrontEnd/lib/main/dashboard.dart
@@ -1,3 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:crud_basic/main/components/button.dart';
+import 'package:crud_basic/main/data/data.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
@@ -8,21 +11,140 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  String getTimer() {
+    if (DateTime.now().hour < 12) {
+      return 'Good Morning';
+    } else if (DateTime.now().hour < 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Evening';
+    }
+  }
+
+  String getUserData() {
+    return 'User';
+  }
+
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
   @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> dataDummy = dataDummymakanan();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              // Add your onPressed code here!
+            },
+          ),
+        ],
+        title: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "${getTimer()}, ${getUserData()} 👋",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Selamat datang di aplikasi QWERTY",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      body: Center(
+      body: Container(
+        margin: EdgeInsets.all(20),
         child: Column(
-          children: <Widget>[
-            Text('Welcome to Dashboard'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: Text('Logout'),
+          children: [
+
+            // Carousel Slider
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 200.0,
+                autoPlay: true,
+                enlargeCenterPage: true,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+              ),
+              items: imgList.map((url) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(
+                          url,
+                          fit: BoxFit.cover,
+                          width: 1000.0,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+
+            
+            // List of Sorting
+            Container(
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Pilih Menu",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                        Text("Sorting sesuai kemauanmu",
+                          style: TextStyle(
+                            fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+            // list of menu
+            Expanded(
+              child: ListView.builder(
+                itemCount: dataDummy.length,
+                itemBuilder: (context, index) {
+                  final menu = dataDummy[index];
+                  return Card(
+                    child: ButtonDashboardShowList(menu: menu),
+                  );
+                },
+              ),
             ),
           ],
         ),
